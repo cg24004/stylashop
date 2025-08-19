f12a5de1b3466c90add6752d03f77cea8f793f2b
Cristian Guzman <cg24004@itcha.edu.sv>
2025-08-19 10:22:07 -0600

    commit initial

---
+++ src/main/java/com/devsoft/Stylashop/StylashopApplication.java
@@ -0,0 +1,13 @@
+package com.devsoft.Stylashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StylashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StylashopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsoft/Stylashop/StylashopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsoft.Stylashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StylashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
